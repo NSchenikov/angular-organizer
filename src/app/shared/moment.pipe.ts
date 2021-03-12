75e8b56cf56425721323c9e37a44bde7827777cc
Nikita Schenikov <nikita-shenikov@mail.ru>
2021-03-12 15:05:38 +0300

    selecting month now working

--- src/app/shared/moment.pipe.ts
+++ src/app/shared/moment.pipe.ts
@@ -2,10 +2,11 @@ import {Pipe, PipeTransform} from '@angular/core';
 import * as moment from 'moment';
 
 @Pipe({
-  name: 'moment'
+  name: 'moment',
+  pure: false
 })
 export class MomentPipe implements PipeTransform {
-  transform(m: moment.Moment, format: string = 'MMMM YYYY'): string {
+  transform(m: moment.Moment, format: string = 'MMMM YYYY'): string { //
     return m.format(format);
   }
 
