9a229ff5007ecaf4872662433c37f18cbd1d31ca
Nikita Schenikov <nikita-shenikov@mail.ru>
2021-03-12 16:19:50 +0300

    calendar table created

--- src/app/shared/moment.pipe.ts
+++ src/app/shared/moment.pipe.ts
@@ -6,7 +6,7 @@ import * as moment from 'moment';
   pure: false
 })
 export class MomentPipe implements PipeTransform {
-  transform(m: moment.Moment, format: string = 'MMMM YYYY'): string { //
+  transform(m: moment.Moment | any, format: string = 'MMMM YYYY'): string { // m type should be only moment.Moment
     return m.format(format);
   }
 
